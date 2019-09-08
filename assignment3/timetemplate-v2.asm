3773f14a50ccf058509d7dace3347a1ca9557d26
Konrad Claesson <konrad.claesson@gmail.com>
2019-09-08 10:02:27 +0200

    push/pop doubly used

--- assignment3/timetemplate-v2.asm
+++ assignment3/timetemplate-v2.asm
@@ -128,10 +128,12 @@ time2string:
 	# Arrange ASCII-coded minutes/seconds into the above specified sequence of
 	# characters in the area of memory pointed to by $t4 = $a0.
 	move 	$a0, $t0	# a0 = NBCD-coded tens of minutes digit
+	#PUSH	($t1)
 	jal	hexasc		# $v0 = ASCII-coded tens of minutes digit
 	nop
 	sb	$v0, 0($t4)	# store 8 least significant bits of $v0 at mem[reg($t4)]
 	
+	#POP	($t1)
 	move	$a0, $t1	# $a0 = NBCD-coded minutes digit
 	jal	hexasc		# $v0 = ASCII-coded minutes digit
 	nop
@@ -150,7 +152,7 @@ time2string:
 				
 	move	$a0, $t3	# $a0 = NBCD-coded seconds digit
 	jal	hexasc		# $v0 = ASCII-coded seconds digit
-	nop
+	nop	
 	sb	$v0, 4($t4)	# store 8 least significant bits of $v0 at
 				# mem[reg($t4 + sgnxt(3)]
 				
